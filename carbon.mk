9d42d45db7fd573175e2b5fe52f76239d2ea8079
Patrick Campanale <patrickappcreator@gmail.com>
2015-05-09 08:27:00 -0700

    Carbon Lollipop bringup
    Change-Id: I2c27032ecf675b9b920e2a6e7808c38c11f543e7

---
+++ carbon.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/hltetmo/full_hltetmo.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+PRODUCT_DEVICE := hltetmo
+PRODUCT_NAME := carbon_hltetmo
